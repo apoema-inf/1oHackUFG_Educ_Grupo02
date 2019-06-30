2e67ef963ec893c0817872f751b827068b9a6549
JrFernando <fernandojunior20110@gmail.com>
2019-06-30 06:01:29 -0300

    Add Página de Configuração

--- monitora-ufg-mobile/src/app/tab3/tab3.page.ts
+++ monitora-ufg-mobile/src/app/tab3/tab3.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+import { NotificacaoFormComponent } from '../shared/notificacao/components/notificacao-form/notificacao-form.component';
+import { MockupService } from '../services/mockup.service';
+import { Usuario } from '../models/usuario';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +12,17 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  public usuario: Usuario;
+
+  constructor(public modalController: ModalController, private service: MockupService) {
+    this.usuario = this.service.usuarioLogado;
+  }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: NotificacaoFormComponent
+    });
+    return await modal.present();
+  }
 
 }
